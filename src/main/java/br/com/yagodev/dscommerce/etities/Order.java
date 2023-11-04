17e99f8ba022b6f7e2ead20a0836664dcede3f3f
Yago Moreira <yagomoreira_dev@yahoo.com>
2023-11-03 23:58:06 -0300

    Order

--- src/main/java/br/com/yagodev/dscommerce/etities/Order.java
+++ src/main/java/br/com/yagodev/dscommerce/etities/Order.java
@@ -17,9 +17,12 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private OrderStatus status;
-    @ManyToMany 
+
+    @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 }
